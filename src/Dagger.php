253be3107288f28c167efae40d40d21f4ee59f51
MazeR <mazer0701@gmail.com>
2020-10-12 23:19:49 +0800

    Dagger add addStringReplaceRule and its test

--- src/Dagger.php
+++ src/Dagger.php
@@ -2,10 +2,37 @@
 
     namespace RewriteDagger;
 
+    use RewriteDagger\CodeRepository\CodeRepositoryInterface;
+
     class Dagger
     {
-        public function testFunction(int $var): int
+        private $codeRepository = null;
+        private $ruleList = [];
+
+        function __construct(CodeRepositoryInterface $codeRepository)
         {
-            return $var;
+            $this->codeRepository = $codeRepository;
+        }
+
+        public function addStringReplaceRule(String $from, String $to): void
+        {
+            $pattern = preg_quote($from, '/');
+            $this->ruleList["/{$pattern}/"] = function () use ($to) {
+                return $to;
+            };
+        }
+
+        public function includeCode(String $path): void
+        {
+            $codeContent = preg_replace_callback_array(
+                $this->ruleList,
+                $this->codeRepository->getCodeContent($path)
+            );
+            if(is_null($codeContent)) {
+                $errorCode = preg_last_error();
+                $errorMessage = preg_last_error_msg();
+                throw new \RuntimeException("preg_replace error code {$errorCode}: {$errorMessage}");
+            }
+            $this->codeRepository->includeCode($codeContent);
         }
     }
---
+++ test/DaggerTest.php
@@ -0,0 +1,38 @@
+<?php declare(strict_types=1);
+
+    use PHPUnit\Framework\TestCase;
+
+    use RewriteDagger\Dagger;
+    use RewriteDagger\CodeRepository\CodeRepositoryInterface;
+
+    // fake CodeRepository that can perceive includeCode input
+    class PerceiveCodeRepository implements CodeRepositoryInterface
+    {
+        public $includeCodeInput = null;
+
+        public function getCodeContent(string $path): string
+        {
+            return '42 is a number.';
+        }
+
+        public function includeCode(string $codeContent): void
+        {
+            $this->includeCodeInput = $codeContent;
+        }
+    }
+
+    final class DaggerTest extends TestCase
+    {
+        public function testAddStringReplaceRule(): void
+        {
+            $codeRepository = new PerceiveCodeRepository();
+            $dagger = new Dagger($codeRepository);
+            $dagger->addStringReplaceRule('$tempGlobalVar', '$tempChangeGlobalVar');
+            $dagger->addStringReplaceRule('is a number', ': Answer to the Ultimate Question of Life, The Universe, and Everything');
+            $dagger->includeCode('');
+            $this->assertSame(
+                '42 : Answer to the Ultimate Question of Life, The Universe, and Everything.',
+                $codeRepository->includeCodeInput
+            );
+        }
+    }
